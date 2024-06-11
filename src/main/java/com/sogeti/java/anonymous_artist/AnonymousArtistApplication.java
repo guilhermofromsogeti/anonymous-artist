c4301b4d0a15e20035e858b72d5364aa7bf890e5
Guilhermo d'Aguiar <guilhermo.d-aguiar@sogeti.com>
2024-06-11 10:06:43 +0200

    first commit for application

---
+++ src/main/java/com/sogeti/java/anonymous_artist/AnonymousArtistApplication.java
@@ -0,0 +1,13 @@
+package com.sogeti.java.anonymous_artist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousArtistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousArtistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sogeti/java/anonymous_artist/AnonymousArtistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogeti.java.anonymous_artist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousArtistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
