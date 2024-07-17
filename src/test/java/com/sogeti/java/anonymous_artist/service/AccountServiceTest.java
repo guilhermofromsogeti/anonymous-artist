acb9eff48fcb6c8b0aaac60801b3a668fcb79c54
Guilhermo d'Aguiar <guilhermo.d-aguiar@sogeti.com>
2024-07-17 15:30:19 +0200

    reports final artifact in github actions

--- src/test/java/com/sogeti/java/anonymous_artist/service/AccountServiceTest.java
+++ src/test/java/com/sogeti/java/anonymous_artist/service/AccountServiceTest.java
@@ -71,7 +71,7 @@ class AccountServiceTest {
         accountService.addAuthority(existingAccount.getEmail(), "ROLE_USER");
 
         // Then
-        verify(accountRepository, times(3)).findById(existingAccount.getEmail());
+        verify(accountRepository, times(1)).findById(existingAccount.getEmail());
         verify(accountRepository, times(1)).save(existingAccount);
         verify(accountRepository).save(any(Account.class));
         verifyNoMoreInteractions(accountRepository);
